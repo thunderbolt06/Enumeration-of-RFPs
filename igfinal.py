a5f7bae988714cf76358947c300304632e8ed7eb
Rahil N Jain <thunderbolt06@github.com>
2019-10-30 11:39:35 +0530

    More graphs

--- igfinal.py
+++ igfinal.py
@@ -31,28 +31,7 @@ def makeBig(glist):
                 for el in garr:
                     if nx.is_isomorphic(el, ga):
                         appen = False
-            
-            
-            if appen:
-                garr.append(ga)
-    return garr
-
-
-
-# ---------------------main---------------------------------------------------------------------------------------------------
-init=4
-g= nx.path_graph(init)
-try:
-    os.mkdir("RFP2/Len={}".format(init))
-except:
-    pass
-ra=1
-nex=[g]
-while(nex):
-    sum=0
-    for x in nex:
-        ga=x.copy()
-        for a1 in ga.nodes():
+            for a1 in ga.nodes():
                 for b1 in ga.nodes():
                     if a1<b1:
                         c = ga.copy()
@@ -63,29 +42,55 @@ while(nex):
                             if a1 in cut and b1 in cut or a1 not in cut and b1 not in cut:
                                 if nx.number_connected_components(c)>2:
                                     appen =False
-        r5 = []
-        for n1 in ga.nodes():
-            e = ga.copy()
-            if ga.degree(n1)>=4:
-                f = e.subgraph([n for n in e.neighbors(n1)])
-                try:
-                    r4 = (list(nx.find_cycle(f, orientation='ignore')))
-                    if len(r4)!=len(f) and len(r4)>=4:
-                        appen= False
-                    else:
-                        r5.append(list(nx.find_cycle(f, orientation='ignore')))
-                except:
-                    pass
-        j1=0
-        for i1 in r5:
-            j1=j1+1
-            for i2 in r5[j1:]:
-                if collections.Counter(i1)==collections.Counter(i2):
-                    appen=False
-        if ga.size()>3*init-7:
-            appen=False
+            r5 = []
+            for n1 in ga.nodes():
+                e = ga.copy()
+                if ga.degree(n1)>=4:
+                    f = e.subgraph([n for n in e.neighbors(n1)])
+                    try:
+                        r4 = (list(nx.find_cycle(f, orientation='ignore')))
+                        if len(r4)!=len(f) and len(r4)>=4:
+                            appen= False
+                        else:
+                            r5.append(list(nx.find_cycle(f, orientation='ignore')))
+                    except:
+                        pass
+            j1=0
+            for i1 in r5:
+                j1=j1+1
+                for i2 in r5[j1:]:
+                    if collections.Counter(i1)==collections.Counter(i2):
+                        appen=False
+            if ga.size()>3*init-7:
+                appen=False
+            
+            if appen:
+                garr.append(ga)
+    return garr
+
+
+
+# ---------------------main---------------------------------------------------------------------------------------------------
+init=5
+g= nx.path_graph(init)
+try:
+    os.mkdir("RFP2")
+except:
+    pass
+try:
+    os.mkdir("RFP2/Len={}".format(init))
+except:
+    pass
+ra=1
+nex=[g]
+while(nex):
+    sum=0
+    for x in nex:
+        appen=True
+        ga=x.copy()
+        
         if appen:
-        sum+=1
+            sum+=1
             plt.figure(ra)
             nx.draw_planar(x,labels=None, font_size=12, font_color='k', font_family='sans-serif', font_weight='normal', alpha=1.0, bbox=None, ax=None)
             plt.savefig('RFP2/Len={}/i{}.png'.format(init,ra))
