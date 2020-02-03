3884d61e1c8b4dc54f926656fc4cd896c179782c
Chinmay Hebbar <4chinmai@gmail.com>
2020-02-03 16:00:17 +0530

    Bug fixes

--- Inner Graph Generator/main.py
+++ Inner Graph Generator/main.py
@@ -3,10 +3,15 @@ import networkx as nx
 # import warnings
 from generate import run_generations
 from plot_graphs import plot_graphs
+from pprint import pprint
 
 # warnings.simplefilter("ignore")
 
 init_len = int(input("Enter the number of vertices: "))
 
 listofgraphs = run_generations(init_len)
-plot_graphs(listofgraphs)
+
+for g in listofgraphs:
+	print(g.nodes)
+
+plot_graphs(init_len, listofgraphs)
