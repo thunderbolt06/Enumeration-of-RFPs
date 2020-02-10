a34c8b8f37c694f63176b02535a305de6fb8708b
Chinmay Hebbar <4chinmai@gmail.com>
2020-02-10 18:15:12 +0530

    Working version IG

--- Inner Graph Generator/plot_graphs.py
+++ Inner Graph Generator/plot_graphs.py
@@ -5,11 +5,14 @@ import networkx as nx
 
 
 def plot_graphs(init_len, graph_list):
-    os.makedirs("./RFP_Graph_Plots/Len {}".format(init_len), True)
+    folder_path = f"RFP_Graph_Plots/Len {init_len}"
+    os.makedirs(folder_path, exist_ok=True)
     graph_no = 1
+    _fig, _ax = plt.subplots()
     for graph in graph_list:
         plt.figure(graph_no)
-        nx.draw_planar(graph, with_labels=True, font_size=12)
-        plt.savefig('Graph_edges_{1} - {2}.png'.format(
-                    init_len, graph.size(), graph_no))
+        nx.draw_planar(graph, labels=None, font_size=12, font_color='k', font_family='sans-serif', font_weight='normal', alpha=1.0, bbox=None, ax=None)
+        # Inner Graph IG_<Initial Vertices>_<Graph_Size>_<Graph No.>.png
+        plt.savefig(f'{folder_path}/IG_{init_len}_{graph.size()}_{graph_no}.png')
         graph_no += 1
+        plt.clf()
