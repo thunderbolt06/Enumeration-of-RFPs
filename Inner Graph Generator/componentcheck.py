587d4097228532e52659060c0cd717e0c9066df0
Chinmay Hebbar <4chinmai@gmail.com>
2020-02-16 15:43:25 +0530

    Fix Component Check

--- Inner Graph Generator/componentcheck.py
+++ Inner Graph Generator/componentcheck.py
@@ -7,33 +7,40 @@ def cip_rule_filter(graph_list):
     Given List of Graphs
     Returns filtered list that satisfy the CIP rule
 
-    CIP Rule = 2 CIP on outer biconnected components and no CIP on
-    inner biconnected components
+    CIP Rule = 2 CIP on outer biconnected components
+                no CIP on inner biconnected components
+                Less than 4 CIPs
     """
     filtered_graphs = []
+    a = 0
     for graph in graph_list:
+        print(f'reach {a}')
+        print(graph.edges())
+        a += 1
         cip_check = True
         outer_comps, inner_comps, single_component = component_break(graph)
         # list, list, element
         # print(outer_comps, inner_comps, single_component)
-        # CIP Rule for Outer Components
-        for comp in outer_comps:
-            print(comp.edges)
-            if num_cips(comp) > 2 and not complex_triangle_check(comp):
+        if not single_component:
+            # CIP Rule for Outer Components
+            for comp in outer_comps:
+                if num_cips(comp) > 2 and not complex_triangle_check(comp):
+                    cip_check = False
+                    print('FALSE')
+            # CIP Rule for Inner Components
+            for comp in inner_comps:
+                if num_cips(comp) > 0 and not complex_triangle_check(comp):
+                    cip_check = False
+                    print('FALSE')
+        else:
+            # CIP Rule for single_component Components
+            if num_cips(single_component) > 4 and \
+                    not complex_triangle_check(single_component):
                 cip_check = False
                 print('FALSE')
-        # CIP Rule for Outer Components
-        for comp in inner_comps:
-            print(comp.edges)
-            if num_cips(comp) > 0 and not complex_triangle_check(comp):
-                cip_check = False
-                print(cip_check)
-        # CIP Rule for single_component Components
-        if num_cips(single_component) > 4 and not complex_triangle_check(comp):
-            cip_check = False
-            print(cip_check)
         if cip_check:
             filtered_graphs.append(graph)
+
     return filtered_graphs
 
 
@@ -69,11 +76,14 @@ def component_break(given_graph):
     [list of other inner components(2 articulation points) with 0 cip]
     """
     test_graph = given_graph.copy()
-    cutvertices = nx.articulation_points(test_graph)
+    cutvertices = list(nx.articulation_points(test_graph))
 
     # O(biconnected_components * cutvertices)
     inner_components = []
     outer_components = []
+    if len(cutvertices) == 0:
+        single_component = test_graph
+        return 0, 0, single_component
     for peice_edges in nx.biconnected_component_edges(test_graph):
         # Find num cutvertices
         # num_cutverts = nx.intersection(peice, cutvertices)
@@ -88,8 +98,6 @@ def component_break(given_graph):
             inner_components.append(peice)
         elif num_cutverts == 1:
             outer_components.append(peice)
-        elif num_cutverts == 0:
-            single_component = peice
         else:
             print("Illegal Case 1")
             print(test_graph.edges())
@@ -97,8 +105,7 @@ def component_break(given_graph):
         if len(outer_components) > 2:
             print("Illegal Case 2")
             print(test_graph)
-
-    return outer_components, inner_components, single_component
+    return outer_components, inner_components, 0
 
 
 def complex_triangle_check(comp):
@@ -126,8 +133,6 @@ def complex_triangle_check(comp):
             outer_vertices.append(edge[0])
         if edge[1] not in outer_vertices:
             outer_vertices.append(edge[1])
-
-    if len(outer_vertices) == 3:
-        if len(comp) != 3:
-            return False
+    if len(outer_vertices) == 3 and len(comp) != 3:
+        return False
     return True
